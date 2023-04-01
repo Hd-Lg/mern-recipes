d6f79eff933cfee858b8dfde19b5f855e0523de0
Hd Lg <59037834+Hd-Lg@users.noreply.github.com>
2023-04-01 15:02:18 +0200

    Working connecting back and front-end

---
+++ client/src/hooks/useGetUserID.js
@@ -0,0 +1,5 @@
+const userGetUserID = () => {
+  return window.localStorage.getItem('userID');
+};
+
+export default userGetUserID;
--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { RecipeModel } from '../models/Recipes.js';
+import { UserModel } from '../models/Users.js';
 
 const router = express.Router();
 
@@ -13,9 +14,19 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Create new recipe
 router.post('/', async (req, res) => {
   // We create the structure of our model with the request body
-  const recipe = new RecipeModel({ ...req.body });
+  const recipe = new RecipesModel({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    image: req.body.image,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    imageUrl: req.body.imageUrl,
+    cookingTime: req.body.cookingTime,
+    userOwner: req.body.userOwner,
+  });
 
   try {
     // We can find in our DB with filters. Here we don't use filters cause we want all of them
@@ -26,4 +37,39 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Save a Recipe
+router.put('/', async (req, res) => {
+  const recipe = await RecipesModel.findById(req.body.recipeID);
+  const user = await UserModel.findById(req.body.userID);
+  try {
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.status(201).json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get id of saved recipes.
+router.get('/savedRecipes/ids', async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    res.json({ savedRecipes: user?.savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// Get saved recipes
+router.get('/savedRecipes', async (req, res) => {
+  try {
+    const user = UserModel.findById(req.body.userID);
+    const savedRecipes = RecipeModel.find({
+      id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
+  } catch (error) {
+    res.json(error);
+  }
+});
 export { router as recipesRouter };
